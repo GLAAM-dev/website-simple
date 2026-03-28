bd4587b2dd504a88090c3663c9e024360980d525
alexanderlin-com <alexanderlinus@gmail.com>
2026-03-28 10:12:16 -0700

    Refactor styles and update resources layout
    - Removed old CSS file and added a new Tailwind CSS configuration in style.css.
    - Updated resources.njk to enhance layout and styling for member resources section.
    - Introduced Prettier configuration for consistent code formatting.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,7 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ "src/public": "/" });
-  
-  // Also pass through standard assets if we create them
+  // Pass through standard assets if we create them
   eleventyConfig.addPassthroughCopy("src/assets");
 
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
